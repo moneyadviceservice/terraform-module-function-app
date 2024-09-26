a6067d40a025ed6470486e13e0bd0f6cec3993b4
Zoe Cope <zoe.cope@hmcts.net>
2024-09-26 16:01:35 +0100

    add function app module

---
+++ main.tf
@@ -0,0 +1,79 @@
+resource "azurerm_service_plan" "this" {
+  count               = var.create_service_plan ? 1 : 0
+  name                = "${var.product}-asp-${var.name}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  os_type             = var.os_type
+  sku_name            = var.sku_name
+}
+
+resource "azurerm_windows_function_app" "this" {
+  count               = var.os_type == "Windows" ? 1 : 0
+  name                = "${var.name}-${var.env}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  storage_account_name       = module.functions_storage_account.name
+  storage_account_access_key = module.functions_storage_account.primary_access_key
+  service_plan_id            = var.create_service_plan != true ? var.service_plan_id : azurerm_service_plan.this[0].id
+
+  app_settings = var.app_settings
+  https_only   = true
+
+  site_config {
+    # application_insights_connection_string = "InstrumentationKey=${module.application_insights.instrumentation_key};IngestionEndpoint=https://uksouth-0.in.applicationinsights.azure.com/"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
+resource "azurerm_linux_function_app" "this" {
+  count               = var.os_type == "Linux" ? 1 : 0
+  name                = "${var.name}-${var.env}"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  storage_account_name       = module.functions_storage_account.name
+  storage_account_access_key = module.functions_storage_account.primary_access_key
+
+  service_plan_id = azurerm_service_plan.this[0].id
+
+  app_settings = var.app_settings
+  https_only   = true
+
+  site_config {
+    # application_insights_connection_string = "InstrumentationKey=${module.application_insights.instrumentation_key};IngestionEndpoint=https://uksouth-0.in.applicationinsights.azure.com/"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+}
+
+module "functions_storage_account" {
+  source = "git@github.com:moneyadviceservice/terraform-module-storage-account?ref=main"
+
+  env                             = var.env
+  storage_account_name            = replace("${var.name}${var.env}", "-", "")
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_kind                    = var.account_kind
+  account_tier                    = var.sa_account_tier
+  account_replication_type        = var.sa_replication_type
+  allow_nested_items_to_be_public = var.allow_nested_items_to_be_public
+  default_action                  = var.default_action
+}
+
+# TODO
+# module "application_insights" {
+#   source = "git@github.com:moneyadviceservice/terraform-module-application-insights?ref=main"
+
+#   env                 = var.env
+#   product             = var.product
+#   name                = "${var.product}-${var.name}"
+#   resource_group_name = var.resource_group_name
+
+# }
---
+++ variables.tf
@@ -0,0 +1,81 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group your function app will be assigned to"
+}
+
+variable "name" {
+  type        = string
+  description = "The name of your function app"
+}
+
+variable "location" {
+  type    = string
+  default = "UK South"
+}
+
+variable "env" {
+  type        = string
+  description = "The environment to deploy to"
+}
+
+variable "app_settings" {
+  type        = map(string)
+  description = "(Optional) A map of key-value pairs for App Settings and custom values."
+}
+
+variable "os_type" {
+  type        = string
+  description = "Required) The O/S type for the App Services to be hosted in this plan."
+  default     = "Linux"
+}
+
+variable "product" {
+  type = string
+}
+
+variable "create_service_plan" {
+  type        = bool
+  description = "If true a new service plan is created"
+  default     = true
+}
+
+variable "service_plan_id" {
+  type        = string
+  description = "If you want to use an existing service plan"
+  default     = null
+}
+variable "account_tier" {
+  type        = string
+  description = "(Required) Defines the Tier to use for this storage account"
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  type        = string
+  description = "(Required) Defines the type of replication to use for this storage account."
+  default     = "ZRS"
+}
+
+variable "account_kind" {
+  type        = string
+  description = "(Optional) Defines the Kind of account"
+  default     = "StorageV2"
+}
+
+variable "sku_name" {
+  type        = string
+  description = "(Required) The SKU for the plan."
+  default     = "Y1"
+}
+
+variable "allow_nested_items_to_be_public" {
+  type        = bool
+  description = "(Optional) Allow or disallow nested items within this Account to opt into being public"
+  default     = false
+}
+
+variable "default_action" {
+  type        = string
+  description = "(Optional) Network rules default action"
+  default     = "Deny"
+}
