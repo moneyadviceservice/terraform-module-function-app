6b4aa1817e0bb38a104ead3e7ec8efa3c3153163
Zoë <zoe.cope@solirius.com>
2025-07-07 16:14:05 +0100

    Update autoscaling.tf

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,1 +1,56 @@
+resource "azurerm_monitor_autoscale_setting" "scale_out" {
+  count               = var.create_service_plan == true ? 1 : 0
+  name                = azurerm_service_plan.this[0].name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  target_resource_id  = azurerm_service_plan.this[0].id
 
+  profile {
+    name = "Scale out condition"
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 6
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.this[0].id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 60
+      }
+
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT5M"
+      }
+    }
+
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_service_plan.this[0].id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 40
+      }
+
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "2"
+        cooldown  = "PT2M"
+      }
+    }
+  }
+}
